41a52e1b97b9cef36153fe9854e6cfacd3f354ea
Harold Barnard <haroldbarnard@gmail.com>
2016-09-20 14:19:58 -0700

    added import function to convert unformated text with file names to python list

--- TomographyTools/reconstruction.py
+++ TomographyTools/reconstruction.py
@@ -15,6 +15,21 @@ import h5py
 
 
 # =============================================================================
+def import_file_list(TextFilePath='../filepath/UnformatedFileList_Test.txt',comment='#'):
+	"""
+	Converts unformatted .txt file with \n separated file names into python list
+	"""
+	textFile = open(TextFilePath,'r')
+	data = textFile.readlines()
+	fileList = []
+	for i in range(len(data)):
+		data[i] = data[i].strip(" ")
+		data[i]=data[i].strip("\n")
+		if data[i][0] != comment:
+			fileList.append(data[i])
+	return(fileList)
+
+# =============================================================================
 def read_als_832h5_metadata(fname):
 	"""
 	Read metadata in ALS 8.3.2 hdf5 dataset files
@@ -75,14 +90,11 @@ def sino_360_to_180(data, overlap=0, rotation='left'):
 	----------
 	data : ndarray
 		Input 3D data.
-
 	overlap : scalar, optional
 		Overlapping number of pixels.
-
 	rotation : string, optional
 		Left if rotation center is close to the left of the
 		field-of-view, right otherwise.
-
 	Returns
 	-------
 	ndarray
@@ -169,7 +181,7 @@ def reconstruct(filename,inputPath="", outputPath="", COR=COR, doOutliers=doOutl
 		# recon_slice == True, only center slice will be reconstructed
 		# if integer is given, a specific 		
 		if recon_slice != False:
-			if (type(recon_slice) == int) and (recon_slice =< numslices):
+			if (type(recon_slice) == int) and (recon_slice <= numslices):
 				sinorange [recon_slice-1, recon_slice]
 			else:
 				sinorange = [numslices//2-1, numslices//2]
@@ -179,7 +191,7 @@ def reconstruct(filename,inputPath="", outputPath="", COR=COR, doOutliers=doOutl
 		# Calculate number of substacks (chunks)
 		substacks = num_substacks #(sinorange[1]-sinorange[0]-1)//num_sino_per_substack+1
 
-		if (sinorange[1]-sinorange[0]) => substacks:
+		if (sinorange[1]-sinorange[0]) >= substacks:
 			num_sino_per_substack = (sinorange[1]-sinorange[0])//num_substacks
 		else:
 			num_sino_per_substack = 1
@@ -251,30 +263,22 @@ def reconstruct(filename,inputPath="", outputPath="", COR=COR, doOutliers=doOutl
 
 
 """
-
 #==============================================================================
 # Reconstruct function with MPI Modifications
 #==============================================================================
-
-
 def reconstructMPI(filename,inputPath="", outputPath="", COR=COR, doOutliers=doOutliers, outlier_diff=outlier_diff, outlier_size=outlier_size, doFWringremoval=doFWringremoval,ringSigma=ringSigma,ringLevel=ringLevel,ringWavelet=ringWavelet,pad_sino=pad_sino,doPhaseRetrieval=doPhaseRetrieval,propagation_dist=propagation_dist,kev=kev,alphaReg=alphaReg,butterworthpars=butterworthpars,doPolarRing=doPolarRing,Rarc=Rarc,Rmaxwidth=Rmaxwidth,Rtmax=Rtmax,Rthr=Rthr,Rtmin=Rtmin,useAutoCOR=useAutoCOR,use360to180=use360to180,num_substacks=num_substacks):
-
 	# get MPI rank for process
  	comm = MPI.COMM_WORLD
 	rank = comm.Get_rank()
-
 	# Convert filename to list type if only one file name is given
 	if type(filename) != list:
 		filename=[filename]
-
 	# If useAutoCor is true, list of COR will be generated with boolean False for entries, this will initiate autoCOR for every dataset
 	if useAutoCOR==True or (len(COR) != len(filename)):
 		logging.info('using auto COR for all input files')
 		COR = [False]*len(filename)
-
 	for x in range(len(filename)):
 		logging.info('opening data set, checking metadata')
-
 		fdata, gdata = read_als_832h5_metadata(inputPath[x]+filename[x]+'.h5')
 		pxsize = float(gdata['pxsize'])/10.0 # convert from metadata (mm) to this script (cm)
 		sinorange = [0, int(gdata['nslices'])]
@@ -287,8 +291,6 @@ def reconstructMPI(filename,inputPath="", outputPath="", COR=COR, doOutliers=doO
 		projs, flat, dark, floc = dxchange.read_als_832h5(inputPath[x]+filename[x]+'.h5', ind_tomo=(firstcor, lastcor))
 		projs = tomopy.normalize_nf(projs, flat, dark, floc)
 		autocor = tomopy.find_center_pc(projs[0], projs[1], tol=0.25)
-
-
 		if (type(COR[x]) == bool) or (COR[x]<0) or (COR[x]=='auto'):
 			firstcor, lastcor = 0, int(gdata['nangles'])-1
 			projs, flat, dark, floc = dxchange.read_als_832h5(inputPath[x]+filename[x]+'.h5', ind_tomo=(firstcor, lastcor))
@@ -296,16 +298,13 @@ def reconstructMPI(filename,inputPath="", outputPath="", COR=COR, doOutliers=doO
 			cor = tomopy.find_center_pc(projs[0], projs[1], tol=0.25)
 		else:
 			cor = COR[x]
-
 		logging.info('Dataset %s, has %d total slices, reconstructing slices %d through %d in %d substack(s), using COR: %f',filename[x], int(gdata['nslices']), sinorange[0], sinorange[1]-1, substacks, cor)
 		
 		for y in range(0, substacks):
 			if (rank == y):
 				logging.info('Starting dataset %s (%d of %d), substack %d of %d',filename[x], x+1, len(filename), y+1, substacks)
-
 				logging.info('Reading sinograms...')
 				projs, flat, dark, floc = dxchange.read_als_832h5(inputPath[x]+filename[x]+'.h5', sino=(sinorange[0]+y*num_sino_per_substack, sinorange[0]+(y+1)*num_sino_per_substack, 1)) 
-
 				logging.info('Doing remove outliers, norm (nearest flats), and -log...')
 				if doOutliers:
 					projs = tomopy.remove_outlier(projs, outlier_diff, size=outlier_size, axis=0)
@@ -324,21 +323,17 @@ def reconstructMPI(filename,inputPath="", outputPath="", COR=COR, doOutliers=doO
 				if doFWringremoval:
 					logging.info('Doing ring (Fourier-wavelet) function...')
 					tomo = tomopy.remove_stripe_fw(tomo, sigma=ringSigma, level=ringLevel, pad=True, wname=ringWavelet)		
-
 				if doPhaseRetrieval:
 					logging.info('Doing Phase retrieval...')
 					tomo = tomopy.retrieve_phase(tomo, pixel_size=pxsize, dist=propagation_dist, energy=kev, alpha=alphaReg, pad=True)	
 					
-
 				logging.info('Doing recon (gridrec) function and scaling/masking, with cor %f...',cor_rec)
 				rec = tomopy.recon(tomo, tomopy.angles(tomo.shape[0], 270, 90), center=cor_rec, algorithm='gridrec', filter_name='butterworth', filter_par=butterworthpars)
 				#rec = tomopy.recon(tomo, tomopy.angles(tomo.shape[0], 180+angularrange/2, 180-angularrange/2), center=cor_rec, algorithm='gridrec', filter_name='butterworth', filter_par=butterworthpars)		
 				rec /= pxsize  # intensity values in cm^-1
-
 				if doPolarRing:  # Apply polar ring filter
 					logging.info('Doing ring (polar mean filter) function...')
 					rec = tomopy.remove_ring(rec, theta_min=Rarc, rwidth=Rmaxwidth, thresh_max=Rtmax, thresh=Rthr, thresh_min=Rtmin)
-
 				if pad_sino: # Un-pad sinogram
 					rec = tomopy.circ_mask(rec[:, npad:-npad, npad:-npad], 0)
 				else:
@@ -348,6 +343,5 @@ def reconstructMPI(filename,inputPath="", outputPath="", COR=COR, doOutliers=doO
 				#dxchange.write_tiff_stack(rec, fname=outputPath+'alpha'+str(alphaReg)+'/rec'+filename[x]+'/rec'+filename[x], start=sinorange[0]+y*num_sino_per_substack)
 				dxchange.write_tiff_stack(rec, fname=outputPath + 'recon_'+filename[x]+'/recon_'+filename[x], start=sinorange[0]+y*num_sino_per_substack)
 			logging.info('Reconstruction Complete: '+ filename[x]+', Process '+rank)
-
 """
 
