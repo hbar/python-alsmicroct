56ba4581e08838a9f484740a0cb1b66fe245a467
Harold S. Barnard <hsbarnard@bl832viz3x.als.lbl.gov>
2017-10-10 17:36:35 -0700

    Added directory to 8bit conversion function and other image processing tools

--- TomographyTools/image_processing.py
+++ TomographyTools/image_processing.py
@@ -1,14 +1,73 @@
 # functions for post processing datasets
-
+import glob
+import numpy as np
+import os
+from skimage import io
+import skimage.external.tifffile as skTiff
 import numexpr as ne # routines for the fast evaluation of array expressions elementwise by using a vector-based virtual machine
 # =============================================================================
 
-def loadDataSet(filepath='./'):
+# generates list of all files in a directory with the desired extension
+def get_fileList(filepath='./', extensions=("tif","tiff")):
+    filepath = filepath.rstrip('/')
+    fileList=[]
+    for iExt in range(len(extensions)):
+        searchterm = filepath+'/*.'+extensions[iExt]
+        files = glob.glob(searchterm)
+        fileList.extend(files)
+    return(fileList)
+
+# loads images in directory into a numpy array
+def load_DataStack(filepath='./',imagerange=(1000,1100)):
     #Imports Tomography Dataset
-    if filepath[-1] !='/':
-        filepath=filepath+"/"
-    fileList = glob.glob(filepath,'*.tiff')
-    return fileList
+    fileList = get_fileList(filepath)
+
+    if imagerange=="all" or imagerange=="All" or imagerange=="ALL":
+        imageMin = 0
+        imageMax = len(fileList)
+    else:
+        imageMin = imagerange[0]
+        imageMax = imagerange[1]
+
+    image = io.imread(fileList[0])
+    rows,cols = image.shape
+
+    dataset = np.zeros((imageMax-imageMin,rows, cols))
+
+    for iImage in range(imageMin,imageMax):
+        dataset[iImage-imageMin,:,:] = io.imread(fileList[iImage])
+        print(iImage)
+    return dataset
+
+
+def convert_DirectoryTo8Bit(inputpath='./', data_min=-10.0, data_max=10.0, outputpath=None,filename=None):
+
+    fileList= get_fileList(inputpath)
+
+    # Strip file extension, etc from first filename in list
+    if filename == None:
+        filename = fileList[0].split('/')[-1]
+        filename = filename.rstrip('.tif')
+        filename = filename.rstrip('0')
+        filename = filename +"_8bit"
+
+    if outputpath == None:
+        outputpath = inputpath.rstrip('/') + "_8bit/"
+
+    # create output directory if it does not exist
+    if not os.path.exists(outputpath):
+        os.makedirs(outputpath)
+
+    for iImage in range(len(fileList)):
+        image32 = io.imread(fileList[iImage])
+        image8 = convert8bit(image32,data_min,data_max)
+        outputfilepath = outputpath.rstrip('/')+'/'+filename + '_' + '{:04d}'.format(iImage) + '.tiff'
+        io.imsave(outputfilepath,image8)
+        print(outputfilepath)
+        #print(iImage)
+
+    print("conversion complete")
+
 
 def convert8bit(rec,data_min,data_max):
     rec = rec.astype(np.float32,copy=False)
@@ -18,3 +77,18 @@ def convert8bit(rec,data_min,data_max):
     ne.evaluate('where(scl<0,0,scl)',out=scl)
     ne.evaluate('where(scl>255,255,scl)',out=scl)
     return scl.astype(np.uint8)
+
+def save_TiffStack(dataset,filename="image",outputPath="./"):
+    filename = filename.rstrip(".tiff")
+    filename = filename.rstrip(".tif")
+    outputFile = outputPath + filename + ".tiff"
+    print("saving ... : " + outputFile)
+    skTiff.imsave(outputFile,dataset)
+    print("save complete: " + outputFile)
+
+def convert_DirectoryToTiffStack(filepath='./'):
+    pass
+
+def convert_ArrayToDirectory(dataset,filename="image",outputPath="./"):
+    pass
+
--- tests/test2_LoadData_8BitConversion.py
+++ tests/test2_LoadData_8BitConversion.py
@@ -2,12 +2,24 @@ import sys
 #sys.path.append("C:/Users/hsbarnard/Dropbox/Research(LBL)/python-TomographyTools")
 sys.path.append("/home/hsbarnard@als.lbl.gov/data-scratch/python/python-TomographyTools/")
 
+import skimage as sk
+from skimage.viewer import ImageViewer
+
 #import TomographyTools as tt
 #import TomographyTools.data_management as dm
 #import TomographyTools.reconstruciton as rn
 import TomographyTools.image_processing as ip
 
-inputDir = "/home/hsbarnard@als.lbl.gov/data-scratch/rec20171005_155036_TiO2_control_scan2/"
+inputDir = "/home/hsbarnard@als.lbl.gov/data-scratch/20171006_ChaiLor_PaintData/rec20171005_162920_TiO2_sample1/"
 
-fileList = ip.loadDataSet(inputDir)
+if True:
+    ip.convert_DirectoryTo8Bit(inputpath=inputDir,data_min=-5.0,data_max=7.0)
 
+if False:
+    imageStack = ip.loadDataStack(inputDir)
+    imageStack8Bit = ip.convert8bit(imageStack,-5.0,7.0)
+
+    ip.saveTiffStack(imageStack,filename="testStack_8Bit")
+
+    image = imageStack8Bit[:,:,50]
+    viewer = ImageViewer(image); viewer.show()
