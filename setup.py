ae423148080b16dae3f703e45d996b490d5c7c67
hbar <haroldbarnard@gmail.com>
2018-06-18 23:07:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='microct_toolbox',
     version='0.1dev',
     author='H.S.Barnard, D.Y.Parkinson',
-    packages=['data_management','image_processing','reconstruction'],
+    packages=['data_management','image_processing','reconstruction',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
