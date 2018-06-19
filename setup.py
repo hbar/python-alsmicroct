a69794f3e6db3a232a8f0abe0b914201205c2ec8
hbar <haroldbarnard@gmail.com>
2018-06-18 23:58:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='microct_toolbox',
     version='0.1dev',
     author='H.S.Barnard, D.Y.Parkinson',
-    packages=['microct_toolbox',],
+    packages=['alsmicroct',],
     license='LICENSE',
     long_description=open('README.md', 'r').read(),
 )
